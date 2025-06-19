03a0574b5cfa91e14e2188a3b38c508908c58824
Luciano Rodriguez <231097lr@gmail.com>
2025-06-19 12:58:58 -0300

    Terminada clase

---
+++ src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '../components/SideMenu.vue';
+
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <!-- Top Menu -->
+        <TopMenu />
+        <main class="flex flex-row">
+            <!-- side menu -->
+            <side-menu />
+            <!-- router view -->
+             <router-view></router-view>
+        </main>
+    </div>
+</template>
---
+++ src/modules/projects/views/ProjectsView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="overflow-x-auto w-full">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th></th>
+          <th>Proyecto</th>
+          <th>Tareas</th>
+          <th>Avances</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="hover:bg-base-300">
+          <th>2</th>
+          <td>Hart Hagerty</td>
+          <td>Desktop Support Technician</td>
+          <td>Purple</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <InputModal />
+
+  <FabButton>
+    <AddCircle />
+  </FabButton>
+</template>
+
+<script setup lang="ts">
+import FabButton from '@/modules/common/components/FabButton.vue';
+import InputModal from '@/modules/common/components/InputModal.vue';
+import AddCircle from '@/modules/common/icons/AddCircle.vue';
+
+</script>
