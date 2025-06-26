ecd1603fd9c819e7ba1dbe59ea8bf304b55d5f2d
Luciano Rodriguez <231097lr@gmail.com>
2025-06-26 12:59:18 -0300

    Clase de tarea

--- src/modules/projects/store/projects.store.ts
+++ src/modules/projects/store/projects.store.ts
@@ -17,7 +17,20 @@ export const useProjectStore = defineStore('projects', () => {
       tasks: []
     });
 
-  }
+  };
+
+  const addTaskToProject = (name: string, id: string) => {
+    if( name.trim().length === 0) return;
+
+    const project = projects.value.find(proj => proj.id === id)
+    if(!project) return;
+
+    project.tasks.push({
+      id: uuidv4(),
+      name,
+      completedAt: null,
+    })
+  };
 
   return {
     // Properties
@@ -28,5 +41,6 @@ export const useProjectStore = defineStore('projects', () => {
     noProjects: computed( () => projects.value.length === 0 ),
     //Actions
     addProject,
+    addTaskToProject,
   }
 })
--- src/modules/projects/views/ProjectView.vue
+++ src/modules/projects/views/ProjectView.vue
@@ -3,14 +3,39 @@
     <section class="m-2">
       <BreadCrumbs :name="project?.name ?? 'No name'" />
     </section>
-    <section>
-      
+    <section class="m-2">
+      <div class="overflow-x-auto">
+        <table class="table">
+          <!-- head -->
+          <thead>
+            <tr>
+              <th class="w-14">Completada</th>
+              <th>Tarea</th>
+              <th>Completada en</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(task, index) in project?.tasks" :key="index" class="hover:bg-base-300">
+              <th>{{ task.id }}</th>
+              <td>{{ task.name }}</td>
+              <td>{{ task.completedAt ?? 'Incompleto' }}</td>
+            </tr>
+            <tr class="hover:bg-base-300">
+              <th></th>
+              <td>
+                <input @keydown.enter="handleAddTask" v-model="taskName" type="text" placeholder="Nueva tarea" class="hover:opacity-100 focus:opacity-100 input input-primary w-full opacity-60 transition-all">
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </section>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import BreadCrumbs from '@/modules/common/components/BreadCrumbs.vue';
 import { useProjectStore } from '../store/projects.store';
 import { useRouter } from 'vue-router';
@@ -22,7 +47,7 @@ import { useRouter } from 'vue-router';
   const router = useRouter();
   const props = defineProps<Props>();
   const projectStore = useProjectStore();
-  // const project = ref<Project | null>();
+  const taskName = ref('');
 
   const project = computed(() => {
     const proj = projectStore.projectList.find( project => project.id === props.id )
@@ -30,7 +55,14 @@ import { useRouter } from 'vue-router';
       router.replace('/')
     }
     return proj
-  })
+  });
+
+
+
+  const handleAddTask = () => {
+    projectStore.addTaskToProject(taskName.value, props.id)
+    taskName.value = '';
+  }
 
   // watch( () => props.id, () => {
   //   project.value = projectStore.projectList.find( project => project.id === props.id )
